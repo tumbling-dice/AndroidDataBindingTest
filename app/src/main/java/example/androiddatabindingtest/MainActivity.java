f982a8e2a6ef940efef487f693cfb09b1d4fabee
tumbling-dice <inujini.dev@gmail.com>
2016-01-30 14:51:20 +0900

    :sushi: very simple data-binding

--- app/src/main/java/example/androiddatabindingtest/MainActivity.java
+++ app/src/main/java/example/androiddatabindingtest/MainActivity.java
@@ -1,13 +1,24 @@
 package example.androiddatabindingtest;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import example.androiddatabindingtest.data.TestData;
+import example.androiddatabindingtest.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+	private ActivityMainBinding _binder;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		_binder = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+		final TestData testData = new TestData(1, "test1");
+
+		_binder.setData(testData);
 	}
 }
---
+++ app/src/main/java/example/androiddatabindingtest/data/TestData.java
@@ -0,0 +1,11 @@
+package example.androiddatabindingtest.data;
+
+public class TestData {
+	public final long id;
+	public final String name;
+
+	public TestData(final long id, final String name) {
+		this.id = id;
+		this.name = name;
+	}
+}
